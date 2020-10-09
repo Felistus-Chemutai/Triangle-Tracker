3ea69fb265361dc7e03baa2ff804eb9fe344eb73
Felistus Chemutai <chemunicole@gmail.com>
2020-10-09 21:37:20 +0300

    add a variable

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 var a, b, c;
 let FindTriangle = () => {
- a = parseInt( document.getElementById('side_a').value)
- b = parseInt(document.getElementById('side_b').value)
- c = parseInt(document.getElementById('side_c').value)
+ a = parseInt( document.getElementById('side_a').value);
+ b = parseInt(document.getElementById('side_b').value);
+ c = parseInt(document.getElementById('side_c').value);
  
 if (((a + b) > c) && ((a + c) > b) &&((b + c) > a)) {
   if ((a === b && a !== c) || (a === c && a !== b) || (b === c && b !== a)) {
