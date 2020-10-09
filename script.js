4fc0a4c6d2a233be29f0b303cee0cfa715a3f818
Felistus Chemutai <chemunicole@gmail.com>
2020-10-09 08:02:08 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+var equal;
+var two eaual;
+var none equal;
+var triangle =["Equilateral","Isosceles", "Scalene"];
+function getTriangle(){
+  value = parsenInt(document.getElementById("value1 ").value;
+   value = parseInt(document.getElementById("value2").value;
+value = parseInt(document.getElementById("value3").value;
+  v = Math.floor(v);
+  var triangles = document.getElementsByName("triangle");
+   if (triangle[0].checked == true) {
+     triangle = " all equal";
+   } else if (triangle[1].checked == true) {
+     triangle = "two equal";
+   }
+   else if (triangle[2].checked == true) {
+     triangle = "none equal";
+      else {
+     return false; }
+
+  switch (triangle) {
+    case "all equal":
+       
+    alert(" This is an:" + triangle[0] +"triangle");}
+    break;
+    switch (triangle) {
+    case "two equal":
+       
+    alert(" This is a:" + triangle[1] +"triangle");}
+    break;
+
+    switch (triangle) {
+    case "none equal":
+       
+   alert(" This is a:" + triangle[2] +"triangle");
+ }
+ break;
+
+default() :
+console.log("done");
+
+       
