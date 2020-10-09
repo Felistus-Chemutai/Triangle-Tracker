458272d72bf07e0af456a51b9748f4ae4e1a778b
Felistus Chemutai <chemunicole@gmail.com>
2020-10-09 20:07:40 +0300

    change

---
+++ js.js
@@ -0,0 +1,21 @@
+var a, b, c
+let FindTriangle = () => {
+ a = parseInt( document.getElementById('side_a').value)
+ b = parseInt(document.getElementById('side_b').value)
+ c = parseInt(document.getElementById('side_c').value)
+ 
+if (((a + b) > c) && ((a + c) > b) &&((b + c) > a)) {
+  if ((a === b && a !== c) || (a === c && a !== b) || (b === c && b !== a)) {
+      alert("This is an Isosceles Triangle")
+   }
+  else if ((a === b) && (b === c)) {
+     alert("This is an Equilateral Triangle");
+   }
+  else {
+      alert("This is a Scalene Triangle");
+   }
+ }
+ else {
+   alert("This is not a triangle");
+ };
+}
